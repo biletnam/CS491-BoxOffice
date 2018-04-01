fbe54707d4c2d67168844f71972659efec256762
Logan <laskinner@crimson.ua.edu>
2018-03-31 22:35:56 -0500

    Added twitter scraper to venv

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,7 +8,7 @@ from twitterscraper import query_tweets
 if __name__ == '__main__':
     count = 0
     with open('output.json','w') as out:
-        for tweet in query_tweets("Black Panther OR blackpanther", None, dt.date(2018,2,14), dt.date(2018,2,16)):
+        for tweet in query_tweets("Black Panther OR blackpanther", None, dt.date(2018,2,9), dt.date(2018,2,16)):
             data = {"text": tweet.text, "replies": tweet.replies, "retweets": tweet.retweets, "likes": tweet.likes}
             out.write('\n')
             json.dump(data,out)
