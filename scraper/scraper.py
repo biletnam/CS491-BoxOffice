3085751f81d1df945bc0cb8ad571c3a03958069f
Logan <laskinner@crimson.ua.edu>
2018-04-04 22:23:16 -0500

    Data for black panther

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,10 +6,12 @@ import datetime as dt
 from twitterscraper import query_tweets
 
 if __name__ == '__main__':
-    count = 0
-    with open('output.json','w') as out:
-        for tweet in query_tweets("Black Panther OR blackpanther", None, dt.date(2018,2,9), dt.date(2018,2,16)):
+    print("Running...")
+    tweet_list = query_tweets("Blade Runner 2049", None, dt.date(2017,10,2), dt.date(2017,10,7))
+    print("Success!")
+    with open('bladerunner.json','w') as out:
+        out.write(str(len(tweet_list)))
+        for tweet in tweet_list:
             data = {"text": tweet.text, "replies": tweet.replies, "retweets": tweet.retweets, "likes": tweet.likes}
             out.write('\n')
             json.dump(data,out)
-
