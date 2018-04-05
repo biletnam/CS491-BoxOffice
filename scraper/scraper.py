d6faf97698165304b8de8b5167decf24f9415cfe
Logan <laskinner@crimson.ua.edu>
2018-04-05 00:24:35 -0500

    Update scraper to use command line args
    Command line args for keyword and date instead of hard coded

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+#Run the program with $python scraper.py string int int int int int int string
+#Command line args are:
+##1. Keyword (ex. "the last jedi")
+##2. Start Year (ex. 2017)
+##3. Start Month (ex. 5)
+##4. Start Day (ex. 27)
+##5. End Year
+##6. End Month
+##7. End Day
+##8. Output file name (ex. "starwars.json")
 
 import sys
 import json
@@ -7,9 +17,9 @@ from twitterscraper import query_tweets
 
 if __name__ == '__main__':
     print("Running...")
-    tweet_list = query_tweets("lego batman", None, dt.date(2017,2,5), dt.date(2017,2,10))
+    tweet_list = query_tweets(sys.argv[1], None, dt.date(int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4])), dt.date(int(sys.argv[5]),int(sys.argv[6]),int(sys.argv[7])))
     print("Success!")
-    with open('legobatman.json','w') as out:
+    with open(sys.argv[8],'w') as out:
         out.write(str(len(tweet_list)))
         for tweet in tweet_list:
             data = {"text": tweet.text, "replies": tweet.replies, "retweets": tweet.retweets, "likes": tweet.likes}
