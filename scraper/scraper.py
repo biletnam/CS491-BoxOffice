d73bc327424f1b603e554bfcce634da0712ec7b0
Logan <laskinner@crimson.ua.edu>
2018-04-04 23:53:54 -0500

    More twitter data...

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,9 +7,9 @@ from twitterscraper import query_tweets
 
 if __name__ == '__main__':
     print("Running...")
-    tweet_list = query_tweets("Blade Runner 2049", None, dt.date(2017,10,2), dt.date(2017,10,7))
+    tweet_list = query_tweets("lego batman", None, dt.date(2017,2,5), dt.date(2017,2,10))
     print("Success!")
-    with open('bladerunner.json','w') as out:
+    with open('legobatman.json','w') as out:
         out.write(str(len(tweet_list)))
         for tweet in tweet_list:
             data = {"text": tweet.text, "replies": tweet.replies, "retweets": tweet.retweets, "likes": tweet.likes}
