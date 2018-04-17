5af5ab54df82d105ddc97042c341fe790e7935d7
smmitchell2 <smmitchell3@gmail.com>
2018-04-16 21:13:28 -0500

    added sentiment analysis to report
    still need to add number of positive negative and neutral

--- analyze.py
+++ analyze.py
@@ -8,11 +8,24 @@ blackPantherFN = 'data/blackpanther.json'
 apesFN = 'data/apes.json'
 lastJediFN = 'data/lastjedi.json'
 legoBatmanFN = 'data/legobatman.json'
+dunkirkFN = 'data/dunkirk.json'
+getoutFN = 'data/getout.json'
+guardiansFN = 'data/guardians.json'
+thorFN = 'data/thor.json'
+waterFN = 'data/water.json'
+wonderFN = 'data/wonder.json'
+arthurFN = 'data/arthur.json'
+baywatchFN = 'data/baywatch.json'
+beautyFN = 'data/beauty.json'
+justiceFN = 'data/justice.json'
 
 def analyzeText(filename):
     tweets_file = open(filename, "r")
     count = 0.0
     sum = 0.0
+    positiveCount = 0
+    negativeCount = 0
+    neutralCount = 0
     for line in tweets_file:
         try:
             # Read in one line of the file, convert it into a json object 
@@ -31,10 +44,30 @@ blackPanther = analyzeText(blackPantherFN)
 apes = analyzeText(apesFN)
 lastJedi = analyzeText(lastJediFN)
 legoBatman = analyzeText(legoBatmanFN)
+arthur = analyzeText(arthurFN)
+baywatch = analyzeText(baywatchFN)
+beauty = analyzeText(beautyFN)
+dunkirk = analyzeText(dunkirkFN)
+getout = analyzeText(getoutFN)
+guardians = analyzeText(guardiansFN)
+justice = analyzeText(justiceFN)
+thor = analyzeText(thorFN)
+water = analyzeText(waterFN)
+wonder = analyzeText(wonderFN)
 
 print 'bladerunner:',bladeRunner
 print 'black panther:',blackPanther
 print 'apes:',apes
 print 'last jedi:',lastJedi
 print 'lego batman:',legoBatman
+print 'arthur:',arthur
+print 'baywatch:',baywatch
+print 'beauty:',beauty
+print 'dunkirk:',dunkirk
+print 'getout:',getout
+print 'guardians:',guardians
+print 'justice:',justice
+print 'thor:',thor
+print 'water:',water
+print 'wonder:',wonder
 
