878a229417f628cc92ee737525ad58dfa41aba7c
smmitchell2 <smmitchell3@gmail.com>
2018-04-17 11:00:53 -0500

    added positive negative and neutral

--- analyze.py
+++ analyze.py
@@ -33,11 +33,17 @@ def analyzeText(filename):
             blob = TextBlob(tweet['text'])
             sum = blob.sentiment.polarity + sum
             count = count + 1.0
+            if blob.sentiment.polarity > 0:
+                positiveCount = positiveCount + 1
+            elif blob.sentiment.polarity == 0:
+                neutralCount = neutralCount + 1
+            else:
+                negativeCount = negativeCount + 1
 
         except:
             # read in a line is not in JSON format (sometimes error occured)
             continue
-    return sum/count
+    return sum/count,positiveCount,negativeCount,neutralCount
 
 bladeRunner = analyzeText(bladeRunnerFilename)
 blackPanther = analyzeText(blackPantherFN)
@@ -55,19 +61,19 @@ thor = analyzeText(thorFN)
 water = analyzeText(waterFN)
 wonder = analyzeText(wonderFN)
 
-print 'bladerunner:',bladeRunner
-print 'black panther:',blackPanther
-print 'apes:',apes
-print 'last jedi:',lastJedi
-print 'lego batman:',legoBatman
-print 'arthur:',arthur
-print 'baywatch:',baywatch
-print 'beauty:',beauty
-print 'dunkirk:',dunkirk
-print 'getout:',getout
-print 'guardians:',guardians
-print 'justice:',justice
-print 'thor:',thor
-print 'water:',water
-print 'wonder:',wonder
+print 'bladerunner:',bladeRunner[0],bladeRunner[1],bladeRunner[2],bladeRunner[3]
+print 'black panther:',blackPanther[0],blackPanther[1],blackPanther[2],blackPanther[3]
+print 'apes:',apes[0],apes[1],apes[2],apes[3]
+print 'last jedi:',lastJedi[0],lastJedi[1],lastJedi[2],lastJedi[3]
+print 'lego batman:',legoBatman[0],legoBatman[1],legoBatman[2],legoBatman[3]
+print 'arthur:',arthur[0],arthur[1],arthur[2],arthur[3]
+print 'baywatch:',baywatch[0],baywatch[1],baywatch[2],baywatch[3]
+print 'beauty:',beauty[0],beauty[1],beauty[2],beauty[3]
+print 'dunkirk:',dunkirk[0],dunkirk[1],dunkirk[2],dunkirk[3]
+print 'getout:',getout[0],getout[1],getout[2],getout[3]
+print 'guardians:',guardians[0],guardians[1],guardians[2],guardians[3]
+print 'justice:',justice[0],justice[1],justice[2],justice[3]
+print 'thor:',thor[0],thor[1],thor[2],thor[3]
+print 'water:',water[0],water[1],water[2],water[3]
+print 'wonder:',wonder[0],wonder[1],wonder[2],wonder[3]
 
